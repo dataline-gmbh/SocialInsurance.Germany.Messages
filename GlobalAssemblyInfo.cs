a98ba14417c543a0cfb7b4821c8a261d2bf3c152
Mark Junker <m.junker@fubar-dev.de>
2015-06-30 11:11:33 +0200

    Added global assembly info file (to ease versioning using a build server)

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Resources;
+
+// Allgemeine Informationen über eine Assembly werden über folgende
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DATALINE GmbH")]
+[assembly: AssemblyCopyright("Copyright © 2015 DATALINE GmbH")]
+//// [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("de")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern
+// durch Einsatz von '*', wie in nachfolgendem Beispiel:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
