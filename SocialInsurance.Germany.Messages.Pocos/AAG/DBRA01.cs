b8b8cf63a5cb08836435372af2db452a93747494
Mark Junker <fubar-coder@live.de>
2015-10-25 03:31:53 +0100

    AAG-Rückmeldungen definiert

---
+++ SocialInsurance.Germany.Messages.Pocos/AAG/DBRA01.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="DBRA01.cs" company="DATALINE GmbH &amp; Co. KG">
+// Copyright (c) DATALINE GmbH &amp; Co. KG. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace SocialInsurance.Germany.Messages.Pocos.AAG
+{
+    /// <summary>
+    /// Datenbaustein: DBBT - Erstattung der Arbeitgeberaufwendungen Beschäftigungsverbot
+    /// </summary>
+    public class DBRA01 : IDatenbaustein
+    {
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="DBRA01"/> Klasse
+        /// </summary>
+        public DBRA01()
+        {
+            KE = "DBRA";
+        }
+
+        /// <summary>
+        /// Holt oder setzt die Kennung
+        /// </summary>
+        /// <remarks>
+        /// Kennung, um welchen Datenbaustein es sich handelt
+        /// </remarks>
+        public string KE { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den Beginn des Erstattungszeitraums
+        /// </summary>
+        /// <remarks>
+        /// Beginn des Erstattungszeitraums, Länge 8, Mussangabe
+        /// </remarks>
+        public DateTime EZEITVOM { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt das Ende des Erstattungszeitraums
+        /// </summary>
+        /// <remarks>
+        /// Ende des Erstattungszeitraums, Länge 8, Mussangabe
+        /// </remarks>
+        public DateTime EZEITBIS { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den beantragten Erstattungsbetrag in der Form EURO/CENT
+        /// </summary>
+        /// <remarks>
+        /// Zulässig ist nur ein Wert größer 0
+        /// </remarks>
+        public int BEBU { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den festgestellten Erstattungsbetrag in der Form EURO/CENT
+        /// </summary>
+        /// <remarks>
+        /// Zulässig ist nur ein Wert größer 0
+        /// </remarks>
+        public int FEBU { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den Grund der Abweichung
+        /// </summary>
+        /// <remarks>
+        /// 01 = Erstattungssatz nicht korrekt
+        /// 02 = Erstattungszeitraum abweichend vom Beschäftigungszeitraum
+        /// 03 = Erstattung U1 über RV-BBG-OST beantragt und auf RV-BBG-OST reduziert (Satzungsregelung)
+        /// 04 = Erstattung U1 über RV-BBG-West beantragt und auf RV-BBG-West reduziert (Satzungsregelung)
+        /// 05 = Kürtung wegen des Bezuges einer Entgeltersatzleistung
+        /// 06 = Erstattungszeitraum fällt in den Wartezeitraum (28 Tage seit Aufnahme der Beschäftigung)
+        /// 07 = Erstattungszeitraum abweichend zum bestehenden EFZ-Anspruch (z.B. Höchstanspruchsdauer überschritten)
+        /// 08 = Erstattung für den ersten Tag der AU beantragt, an dem aber noch gearbeitet wurde
+        /// 09 = Erstattungszeitraum abweichend zum Mutterschaftsgeldzeitraum
+        /// 10 = Mutterschaftsgeld nicht berücksichtigt
+        /// 11 = GSV-Beitrag im Erstattungsbetrag nicht pauschal berücksichtigt
+        /// 12 = GSV-Beitrag im Erstattungsbetrag nicht in tatsächlicher Höhe berücksichtigt
+        /// 13 = Antrag umfasste bereits erstattete Zeiträume
+        /// 14 = Sonstiges
+        /// </remarks>
+        public int GAB { get; set; }
+    }
+}
---
+++ SocialInsurance.Germany.Messages.Pocos/DBAP01.cs
@@ -0,0 +1,122 @@
+﻿// <copyright file="DBAP01.cs" company="DATALINE GmbH &amp; Co. KG">
+// Copyright (c) DATALINE GmbH &amp; Co. KG. All rights reserved.
+// </copyright>
+
+namespace SocialInsurance.Germany.Messages.Pocos
+{
+    public class DBAP01 : IDatenbaustein
+    {
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="DBAP01"/> Klasse
+        /// </summary>
+        public DBAP01()
+        {
+            KE = "DBAP";
+        }
+
+        /// <summary>
+        /// Holt oder setzt die Kennung
+        /// </summary>
+        /// <remarks>
+        /// Kennung, um welchen Datenbaustein es sich handelt
+        /// </remarks>
+        public string KE { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt das Geschlecht zur Anrede des Ansprechpartners
+        /// </summary>
+        /// <remarks>
+        /// Geschlecht zur Anrede des Ansprechpartners, Länge 1, Pflichtangabe, soweit bekannt
+        /// M = Männlich, W = Weiblich, S = Sonstiges
+        /// </remarks>
+        public string ANRAP { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den Namen des Ansprechpartners
+        /// </summary>
+        /// <remarks>
+        /// Name des Ansprechpartners, Länge 30, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string NAMEAP { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt die Rufnummer des Ansprechpartners
+        /// </summary>
+        /// <remarks>
+        /// Rufnummer des Ansprechpartners, Länge 20, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string TELAP { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt die Faxrufnummer des Ansprechpartners
+        /// </summary>
+        /// <remarks>
+        /// Faxrufnummer des Ansprechpartners, Länge 20, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string FAXAP { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt die E-Mail-Adresse des Ansprechpartners
+        /// </summary>
+        /// <remarks>
+        /// E-Mail-Adresse des Ansprechpartners, Länge 70, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string EMAILAP { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den Namen des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Name des Erstellers der Datei, Länge 30, Mussangabe
+        /// </remarks>
+        public string NAME1 { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den zweiten Namensbestandteil des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Zweiter Namensbestandteil des Erstellers der Datei, Länge 30, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string NAME2 { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den dritten Namensbestandteil des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Dritter Namensbestandteil des Erstellers der Datei, Länge 30, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string NAME3 { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt die Postleitzahl des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Postleitzahl des Erstellers der Datei, Länge 10, Mussangabe
+        /// </remarks>
+        public string PLZ { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt den Betriebssitz des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Betriebssitz des Erstellers der Datei, Länge 34, Mussangabe
+        /// </remarks>
+        public string ORT { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt die Straße des Betriebssitzes des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Straße des Betriebssitzes des Erstellers der Datei, Länge 33, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string STR { get; set; }
+
+        /// <summary>
+        /// Holt oder setzt die Hausnummer des Betriebssitzes des Erstellers der Datei
+        /// </summary>
+        /// <remarks>
+        /// Hausnummer des Betriebssitzes des Erstellers der Datei, Länge 9, Pflichtangabe, soweit bekannt
+        /// </remarks>
+        public string NR { get; set; }
+    }
+}
