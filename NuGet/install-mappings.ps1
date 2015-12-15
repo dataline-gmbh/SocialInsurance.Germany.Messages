f2e9329fd9dbf4aee2ad1e0eac3a06608461ebd5
Mark Junker <m.junker@fubar-dev.de>
2015-12-15 17:26:57 +0100

    lib-Pfad korrigiert

--- NuGet/install-mappings.ps1
+++ NuGet/install-mappings.ps1
@@ -3,7 +3,7 @@ param($installPath, $toolsPath, $package, $project)
 $getTargetFrameworkMoniker = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_TargetFrameworkMoniker")
 $targetFrameworkVersion = (New-Object System.Runtime.Versioning.FrameworkName $getTargetFrameworkMoniker.Invoke($project.Object, $null)).Version
 
-$fwPath = "portable-net45+win+wpa81+MonoAndroid10+MonoTouch10+Xamarin.iOS10"
+$fwPath = "portable-net45+dnxcore50+win+wpa81+MonoAndroid10+xamarinios10+MonoTouch10"
 
 $assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\$fwPath\SocialInsurance.Germany.Messages.Mappings.dll")
 $obj = $project.Object
