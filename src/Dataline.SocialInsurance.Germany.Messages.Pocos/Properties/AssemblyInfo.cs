ea47fadd764bc13c32c69bb123a27f5287edee5a
Jan Hindermann <j.hindermann@dataline.de>
2018-05-03 15:10:55 +0200

    Projekt-Metadaten neu organisiert

--- src/Dataline.SocialInsurance.Germany.Messages.Pocos/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// <copyright file="AssemblyInfo.cs" company="DATALINE GmbH &amp; Co. KG">
-// Copyright (c) DATALINE GmbH &amp; Co. KG. All rights reserved.
-// </copyright>
-
-using System.Reflection;
-
-[assembly: AssemblyTitle("SV-Meldungen für Deutschland")]
-[assembly: AssemblyDescription("Klassen für SV-Meldungen für Deutschland")]
