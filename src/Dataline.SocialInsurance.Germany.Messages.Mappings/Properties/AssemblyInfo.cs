feed1ec178ae00adcfca66667dfd996688685ca8
Jan Hindermann <j.hindermann@dataline.de>
2018-05-24 10:24:20 +0200

    CLSCompliant-Attribute wieder hinzugefügt

---
+++ src/Dataline.SocialInsurance.Germany.Messages.Mappings/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿// <copyright file="AssemblyInfo.cs" company="DATALINE GmbH &amp; Co. KG">
+// Copyright (c) DATALINE GmbH &amp; Co. KG. All rights reserved.
+// </copyright>
+
+[assembly: System.CLSCompliant(true)]
