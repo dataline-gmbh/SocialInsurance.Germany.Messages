7ab79799335161f7c2302a00a05e5f2c5b54b8f4
Jan Hindermann <j.hindermann@dataline.de>
2019-04-18 10:51:41 +0200

    Tests für EEL hinzugefügt

---
+++ test/Dataline.SocialInsurance.Germany.Messages.Tests/EEL/EELTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using BeanIO;
+using SocialInsurance.Germany.Messages.Pocos;
+using SocialInsurance.Germany.Messages.Pocos.EEL;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SocialInsurance.Germany.Messages.Tests.EEL
+{
+    public class EELTests : TestBasis2, IClassFixture<DefaultStreamFactoryFixture>
+    {
+        public EELTests(DefaultStreamFactoryFixture fixture, ITestOutputHelper output)
+            : base(fixture.Factory, output)
+        {
+        }
+
+        [Theory]
+        [InlineData("TEEK0101_0166_122018")]
+        [InlineData("TEEK0102_0161_122018")]
+        [InlineData("TEEK0103_0166_042019")]
+        [InlineData("TEEL_DSLW_AlleBausteine")]
+        public void EELRueckmeldung(string filename)
+        {
+            var ds = GetDatensaetze(filename);
+            AssertDatensatzCollection<DSLW09>(ds);
+        }
+    }
+}
---
+++ test/Dataline.SocialInsurance.Germany.Messages.Tests/TestBasis2.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using BeanIO;
+using SocialInsurance.Germany.Messages.Pocos;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SocialInsurance.Germany.Messages.Tests
+{
+    public abstract class TestBasis2 : TestBasis
+    {
+        protected TestBasis2(StreamFactory factory)
+            : this(factory, null)
+        { }
+
+        protected TestBasis2(StreamFactory factory, ITestOutputHelper output)
+            : base(factory)
+        {
+            TestOutput = output ?? new NullOutput();
+        }
+
+        protected ITestOutputHelper TestOutput { get; }
+
+        protected IReadOnlyCollection<IDatensatz> GetDatensaetze(string filename)
+        {
+            using (var reader = StreamFactory.CreateReader("super-message", LoadData(filename)))
+            {
+                var dsList = new List<IDatensatz>(3);
+                object ds;
+                try
+                {
+                    while ((ds = reader.Read()) != null)
+                    {
+                        dsList.Add(ds as IDatensatz);
+                    }
+                }
+                catch (BeanReaderException beanReaderEx)
+                {
+                    ReaderErrorHandler(beanReaderEx);
+                    throw;
+                }
+
+                return dsList;
+            }
+        }
+
+        private void ReaderErrorHandler(BeanReaderException exception)
+        {
+            TestOutput.WriteLine("Fehler beim Lesen der Meldung: {0}", exception.Message);
+            var ctx = exception.RecordContext;
+            TestOutput.WriteLine("Datensatzfehler vorhanden: {0}, Feldfehler vorhanden: {1}", ctx.HasRecordErrors, ctx.HasFieldErrors);
+            if (ctx.HasRecordErrors)
+            {
+                TestOutput.WriteLine("  Datensatzfehler ({0}):", ctx.RecordErrors.Count);
+                foreach (var error in ctx.RecordErrors)
+                {
+                    TestOutput.WriteLine("    {0}", error);
+                }
+            }
+            if (ctx.HasFieldErrors)
+            {
+                var fieldErrors = ctx.GetFieldErrors();
+                TestOutput.WriteLine("  Feldfehler ({0}):", fieldErrors.Count);
+                foreach (var error in fieldErrors)
+                {
+                    TestOutput.WriteLine("    {0}:", error.Key);
+                    foreach (var detail in error)
+                    {
+                        TestOutput.WriteLine("      {0}", detail);
+                    }
+                }
+            }
+        }
+
+        protected void AssertDatensatzCollection<TDatensatz>(IReadOnlyCollection<IDatensatz> datensaetze)
+            where TDatensatz : class, IDatensatz
+        {
+            Assert.NotEmpty(datensaetze);
+            Assert.Equal(3, datensaetze.Count);
+            Assert.Collection(datensaetze,
+                o => Assert.IsType<VOSZ>(o),
+                o => Assert.IsType<TDatensatz>(o),
+                o => Assert.IsType<NCSZ>(o)
+            );
+        }
+
+        private sealed class NullOutput : ITestOutputHelper
+        {
+            internal NullOutput()
+            { }
+
+            void ITestOutputHelper.WriteLine(string message)
+            { /* nichts */ }
+
+            void ITestOutputHelper.WriteLine(string format, params object[] args)
+            { /* nichts */ }
+        }
+    }
+}
