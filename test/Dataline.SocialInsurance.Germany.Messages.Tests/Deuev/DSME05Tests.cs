3b6281a3928adf374abc61607864cd442125b664
Jan Hindermann <j.hindermann@dataline.de>
2019-04-24 15:49:52 +0200

    Test für DSMEv05

---
+++ test/Dataline.SocialInsurance.Germany.Messages.Tests/Deuev/DSME05Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using BeanIO;
+using SocialInsurance.Germany.Messages.Pocos;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SocialInsurance.Germany.Messages.Tests.Deuev
+{
+    public class DSME05Tests : TestBasis2, IClassFixture<DefaultStreamFactoryFixture>
+    {
+        public DSME05Tests(DefaultStreamFactoryFixture fixture, ITestOutputHelper output)
+            : base(fixture.Factory, output)
+        {
+        }
+
+        [Theory]
+        [InlineData("AlleBausteine.dat")]
+        public void TestParseDSME05(string filename)
+        {
+            string semiQualifiedPath = $"DSME05.{filename}";
+            var ds = GetDatensaetze(semiQualifiedPath);
+            AssertDatensatzCollection<DSME05>(ds);
+            TestRoundtripFile(semiQualifiedPath, ds);
+        }
+    }
+}
